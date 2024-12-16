eb3210b60bb6b20efe6e386dc739ecf940a36229
benKosters <bak32@calvin.edu>
2024-12-16 02:22:58 -0500

    fix minor merge conflict issue

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -338,9 +338,12 @@ function MainPage() {
     return ((totalBalance - 10000) / totalBalance) * 100;
   };
 
-  const calculateStockPerformance = (stock) => {
-    return ((Object.values(stock.Prices[stock.Prices.length - 1])[0] - Object.values(stock.Prices[stock.Prices.length - 2])[0]) / Object.values(stock.Prices[stock.Prices.length - 1])[0]) * 100;
-  }
+  // const calculateStockPerformance = (stock) => {
+  //   return ((Object.values(stock.Prices[stock.Prices.length - 1])[0] -
+  //     Object.values(stock.Prices[stock.Prices.length - 2])[0]) /
+  //     Object.values(stock.Prices[stock.Prices.length - 1])[0]) * 100;
+  // }
+
 
   // Calculate the overall performance
   const overallPerformance = calculateOverallPerformance();
